a12f9b2cb45380b8a9e1ab5e9f26a6f05cf82a38
Andreas Holstenson <a@holstenson.se>
2019-08-27 13:27:13 +0200

    feat: `GraphQLNonNull` annotation

---
+++ graphql-binding/src/main/java/se/l4/graphql/binding/annotations/GraphQLNonNull.java
@@ -0,0 +1,17 @@
+package se.l4.graphql.binding.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Set that a field, method or parameter never returns {@code null}.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER })
+public @interface GraphQLNonNull
+{
+}

a6a1b554a6f1f7fb3f7c54614b2a712aa0b19dd0
Andreas Holstenson <a@holstenson.se>
2021-01-27 09:35:41 +0100

    fix: GraphQLDeprecated can be applied to parameters

--- graphql-binding/src/main/java/se/l4/graphql/binding/annotations/GraphQLDeprecated.java
+++ graphql-binding/src/main/java/se/l4/graphql/binding/annotations/GraphQLDeprecated.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.FIELD, ElementType.METHOD })
+@Target({ ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER })
 public @interface GraphQLDeprecated
 {
 	/**
